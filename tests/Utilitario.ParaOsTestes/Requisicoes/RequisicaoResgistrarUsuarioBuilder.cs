3b82b033a039554a1a0d83de409045801ef8b13a
PatricioSouza <patriciozebra3@gmail.com>
2023-08-25 14:54:54 -0300

    criando testes de unidade para o validator de registrar usuario

---
+++ tests/Utilitario.ParaOsTestes/Requisicoes/RequisicaoResgistrarUsuarioBuilder.cs
@@ -0,0 +1,22 @@
+﻿using Bogus;
+using MeuLivroDeReceitas.Comunicacao.Requisicoes;
+
+namespace Utilitario.ParaOsTestes.Requisicoes;
+
+
+public class RequisicaoResgistrarUsuarioBuilder {
+
+    public static RequisicaoRegistrarUsuarioJson Construir(int tamanho = 10) {
+
+        return new Faker<RequisicaoRegistrarUsuarioJson>()
+            .RuleFor(requisicao => requisicao.Nome, f => f.Person.FullName)
+            .RuleFor(requisicao => requisicao.Email, f => f.Internet.Email())
+            .RuleFor(requisicao => requisicao.Senha, f => f.Internet.Password(tamanho))
+            .RuleFor(
+                requisicao => requisicao.Telefone, f => f.Phone.PhoneNumber(
+                    "!#9########"
+                ).Replace("!", $"{f.Random.Int(min: 1, max: 9)}")
+            );
+    }
+
+}
