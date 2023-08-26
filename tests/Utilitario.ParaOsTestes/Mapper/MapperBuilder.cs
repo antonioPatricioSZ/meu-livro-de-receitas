c4b26ed83e8675384a3b13f48943030b5f59e543
PatricioSouza <patriciozebra3@gmail.com>
2023-08-26 11:39:35 -0300

    adicionando testes de unidade de usecase registrar usuario

---
+++ tests/Utilitario.ParaOsTestes/Mapper/MapperBuilder.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MeuLivroDeReceitas.Application.Servicos.Automapper;
+
+namespace Utilitario.ParaOsTestes.Mapper;
+
+
+
+public class MapperBuilder
+{
+    public static IMapper Instancia()
+    {
+
+        var mockMapper = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile(new AutoMapperConfiguracao());
+        });
+        return mockMapper.CreateMapper();
+    }
+}
